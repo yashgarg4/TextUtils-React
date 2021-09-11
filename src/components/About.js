8c64c34c93b48422097d598d04f1258319982ab1
yashgarg4 <yashgarg04072001@gmail.com>
2021-09-11 16:10:36 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // })
+
+    let myStyle = {
+      color: props.mode === 'dark'?'white':'#042743',
+      backgroundColor: props.mode === 'dark'?'#244b68':'white'
+    }
+
+  return (
+    <div className="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
+        <h1 className="my-3">About Us</h1>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item" >
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+             <strong>Analyze you text</strong> 
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils gives you a way to analyze your text quickly and efficiently. Be it word cound,character count ,etc.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+            <strong>Free to Use</strong>  
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextUtils is a free character counter tool taht provides instant character count & word count statistics fro a given text. TextUtils reports the number of words and characters. Thus it is suitable for Writing text with word/character limit.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+            <strong>Browser Compatible</strong>  
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              This word counter software works in any web browsers such as chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books , excel document, pdf document, essays, etc. 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import { Link } from "react-router-dom";
+
+export default function Navbar(props) {
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          {props.title}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              {/* <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link> */}
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            
+          </ul>
+          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>  
+          {/* text-light is bootstrap cls to make any text light. Backticks is used to make js strings and use template literals. $ lga ke koi bhi variable use kr skte h hm.*/}
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+          </div>
+            
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+    title: 'Set title here',
+    aboutText: 'About'
+};
